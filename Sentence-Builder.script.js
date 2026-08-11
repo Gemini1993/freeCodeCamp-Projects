2832de03fd5f51a8c54378b6d2eb6abb3978787f
Emily Theisen <NtrovertedNspiration@gmail.com>
2026-08-10 23:14:40 -0500

    Create Sentence Builder with two stories
    Added two whimsical stories using variables for adjectives, nouns, verbs, and places.

---
+++ Sentence-Builder.script.js
@@ -0,0 +1,19 @@
+let adjective = "friendly";
+let noun = "dragon";
+let verb = "laughing";
+let place = "enchanted forest";
+let adjective2 = "glowing";
+let noun2 = "tacos";
+
+let firstStory = "Once upon a time, there was a(n) " + adjective + " " + noun + " who loved to eat " + noun2 + ". The " + noun + " lived in a " + place + " and had " + adjective2 + " nostrils that blew fire when it was " + verb + ".";
+console.log("First story: " + firstStory);
+
+adjective = "grumpy";
+noun = "wizard";
+verb = "sneezing";
+place = "spooky castle";
+adjective2 = "tiny";
+noun2 = "marshmellows";
+
+let secondStory = "Once upon a time, there was a(n) " + adjective + " " + noun + " who loved to eat " + noun2 + ". The " + noun + " lived in a " + place + " and had " + adjective2 + " nostrils that blew fire when it was " + verb + ".";
+console.log("Second story: " + secondStory);
