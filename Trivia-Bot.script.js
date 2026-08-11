7f97b31204bfa4e4ca84b46f9c667f2eaad19d3a
Emily Theisen <NtrovertedNspiration@gmail.com>
2026-08-10 23:13:13 -0500

    Add Trivia Bot script with coding facts

---
+++ Trivia-Bot.script.js
@@ -0,0 +1,21 @@
+console.log("Hello! I'm your coding fun fact guide!");
+ let bot;
+
+const botName = "Aria";
+const botLocation = "the endless internet";
+const favoriteLanguage = "JavaScript";
+
+console.log("My name is " + botName + " and I live on " + botLocation + ".");
+
+console.log("My favorite programming language is " + favoriteLanguage + ".");
+
+let codingFact = "I like " + favoriteLanguage + " because it makes HTML and CSS become interactive.";
+console.log(codingFact);
+
+codingFact = favoriteLanguage + " was created in just 10  days!";
+console.log(codingFact);
+
+codingFact = favoriteLanguage + " runs on both browsers and on servers using Node.js.";
+console.log(codingFact);
+
+console.log("It was fun sharing these facts with you. Goodbye! - " + botName + " from " + botLocation + ".");
